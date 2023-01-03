6589071f7eb5b28e9e8ec897761dcf355bdc8091
slow-wave <memoon_mj@icloud.com>
2023-01-03 18:28:48 +0900

    add favorite page

---
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -0,0 +1,79 @@
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import './favorite.css'
+import { Popover } from 'antd'
+import { IMAGE_BASE_URL } from '../../Config'
+
+function FavoritePage() {
+    const [Favorites, setFavorites] = useState([])
+
+    useEffect(()=> {
+        fetchFavoredMovie()
+    },[])
+
+    const fetchFavoredMovie = () => {
+        Axios.post('/api/favorite/getFavoredMovie', { userFrom: localStorage.getItem('userId')})
+        .then(response => {
+            if(response.data.success) {
+                setFavorites(response.data.favorites)
+            }else {
+                alert("영화 정보 가져오기 실패")
+            }
+        })
+
+    }
+    const onClickDelete = (movieId, userFrom) => {
+        const variables = {
+            movieId,
+            userFrom
+        }
+
+        Axios.post('/api/favorite/removeFromFavorite', variables)
+        .then(response => {
+            if(response.data.success) {
+                fetchFavoredMovie()
+            }else {
+                alert("favorite 삭제 실패")
+            }
+        })
+    } 
+
+    const renderCards = Favorites.map((favorite, index) => {
+        const content = (
+            <div>
+                {favorite.moviePost ?
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`}/>:"no image"}
+            </div>
+        )
+        return <tr key={index}>
+            <Popover content={content} title={`${favorite.movieTitle}`}>
+                <td>{favorite.movieTitle}</td>
+            </Popover>
+            <td>{favorite.movieRunTime}</td>
+            <td><button onClick={() =>onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
+        </tr>
+    })
+
+
+    return (
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <h2> Favorite Movies </h2>
+            <hr/>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Movie Title</th>
+                        <th>Movie RunTime</th>
+                        <th>Remove from Favorite</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   {renderCards}
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default FavoritePage
