37e602182b62f8793cd933d20631193d1f867284
slow-wave <memoon_mj@icloud.com>
2023-01-08 21:14:46 +0900

    change logo image

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Menu from './Sections/Menu';
-import { Drawer, Button } from 'antd';
-import Icon from '@ant-design/icons';
 import './Sections/Navbar.css';
+import logo from './Sections/logo.png'
 
 function NavBar() {
-  const [visible, setVisible] = useState(false)
-
   return (
     <nav className="menu" style={{ zIndex: 5, width: '100%' }}>
       <div className="menu__logo">
-        <a href="/">Logo</a>
+        <a href="/"><img src={logo} alt="logo" style={{width:'50px'}}/></a>
       </div>
       <div>
         <Menu />
