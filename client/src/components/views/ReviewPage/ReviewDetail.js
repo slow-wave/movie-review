a94b3b9077a371f10faff425e65374464d41cd56
slow-wave <memoon_mj@icloud.com>
2023-01-10 23:33:36 +0900

    update RevewDetail page design

--- client/src/components/views/ReviewPage/ReviewDetail.js
+++ client/src/components/views/ReviewPage/ReviewDetail.js
@@ -1,18 +1,18 @@
 import Axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
-import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import RatingPage from './Sections/Rating'
-import { Button, Tag } from 'antd';
-
+import { Button, Tag, Card, Space, Typography } from 'antd';
+const { Text } = Typography;
 function ReviewDetail(props) {
     const [Review, setReview] = useState([])
     const [bestScore, setbestScore] = useState(0)
     const [Tags, setTags] = useState([])
     
     const location = useLocation();
-    const movieId = location.state.movieId
-    
+    const image = location.state.image
+    const movieName = location.state.movieName
+
     let reviewId = props.match.params.reviewId
 
     useEffect(()=> {
@@ -46,33 +46,42 @@ function ReviewDetail(props) {
     return (
         <div>
             <div style = {{ width: '100%', margin: '0'}}>
-                <div style = {{ width: '85%', margin: '1rem auto'}}><h2>Review</h2><hr /></div>
-                <div style = {{ width: '85%', margin: '1rem auto'}}>
-                    <h3>Movie Info</h3>
-                    <a href={`/movie/${Review.movieId}`}>link</a>
-                <hr /></div>
-                <div style = {{ width: '85%', margin: '1rem auto'}}>
-                    <h3>Star Ratings</h3>
-                    <RatingPage
-                        showRating
-                        setbestScore={setbestScore}
-                        score={Review.ratingTotal}
-                    />
-                <hr /></div>
-                <div style = {{ width: '85%', margin: '1rem auto'}}>
-                    <h3>Tags</h3>
-                    { Tags && Tags.map((tag, index) => (
-                        <Tag>{tag.tagName}</Tag>
-                    ))}
-                <hr /></div>
-                <div style = {{ width: '85%', margin: '1rem auto'}}>
-                    <h3>Review</h3>
-                    <h4>one-line</h4>
-                    <p>{Review.comment}</p>
+                <div style = {{ width: '85%', margin: '1rem auto'}}><h2>Review</h2><hr />
+                    <div style={{display:'flex'}}>
+                        <div style = {{ flex:'1'}}>
+                            <h3>Movie Info</h3>
+                            <a href={`/movie/${Review.movieId}`}>
+                                <img style={{ width:'30%'}} src={image} alt={movieName}/>
+                            </a>
+                        </div>
+                        <div style={{flex:'1'}}>
+                            <div style = {{ width: '85%', margin: '1rem auto'}}>
+                                <h3><Text mark>Star Ratings</Text></h3>
+                                <RatingPage
+                                    showRating
+                                    setbestScore={setbestScore}
+                                    score={Review.ratingTotal}
+                                />
+                            </div>
+                            <div style = {{ width: '85%', margin: '1rem auto'}}>
+                                <h3><Text mark>Tags</Text></h3>
+                                { Tags && Tags.map((tag, index) => (
+                                    <Tag>{tag.tagName}</Tag>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div style = {{ width: '85%', margin: '1rem auto'}}>
-                    <h4>detail</h4>
-                    <p>{Review.mainContent}</p>
+                    <h3><Text mark>Review</Text></h3>
+                    <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                        <Card title="one-line">
+                            <p>{Review.comment}</p>
+                        </Card>
+                        <Card title="detail">
+                            <p>{Review.mainContent}</p>
+                        </Card>
+                    </Space>
                 </div>
                 <div style = {{ width: '85%', margin: '1rem auto'}}>
                     <Button>수정</Button>
