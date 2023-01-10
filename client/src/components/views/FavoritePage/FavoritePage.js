3dabaff3849ac282fe7f34f1e49050333aade0fd
slow-wave <memoon_mj@icloud.com>
2023-01-10 22:53:15 +0900

    update favorite page design

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -2,8 +2,9 @@ import './favorite.css'
 import Axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { IMAGE_BASE_URL } from '../../Config'
+import { Link } from 'react-router-dom';
 import { Popover, Button } from 'antd'
-import { EditOutlined } from '@ant-design/icons';
+import { EditOutlined, CloseCircleTwoTone, PlusSquareTwoTone, RightSquareTwoTone } from '@ant-design/icons';
 
 function FavoritePage() {
     const [Favorites, setFavorites] = useState([]);
@@ -50,18 +51,25 @@ function FavoritePage() {
             <Popover content={content} title={`${favorite.movieTitle}`}>
                 <td>{favorite.movieTitle}</td>
             </Popover>
-            <td>{favorite.movieRunTime}</td>
-            <td><Button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</Button></td>
             <td>
                 {/* 작성된 리뷰가 없다면 '새로 작성' 활성화 */}
                 {(favorite.detailed.length == 0 &&
-                    <Button href={`/review/submit/${favorite.movieId}`}>새로 작성<EditOutlined /></Button>
+                    <a href={`/review/submit/${favorite.movieId}`}>
+                        <PlusSquareTwoTone twoToneColor='#30A9DE' style={{fontSize:'20px'}}/>
+                        {/* <Button href={`/review/submit/${favorite.movieId}`}>새로 작성<EditOutlined /></Button> */}
+                    </a>
                 )} 
                 {/* 작성된 리뷰가 있다면 '리뷰 확인' 활성화 */}
-                {(favorite.detailed.length != 0 &&
-                    <Button href={`/review/${favorite.detailed[0]._id}`}>리뷰 확인</Button>
+                {(favorite.detailed.length !== 0 &&
+                    <Link to={{pathname: `/review/${favorite.detailed[0]._id}`, 
+                        state:{ image: `${IMAGE_BASE_URL}w500${favorite.detailedMovie[0].poster_path}`,
+                        movieName: favorite.detailedMovie[0].original_title}}}> 
+                        <RightSquareTwoTone twoToneColor="#A593E0" style={{fontSize:'20px'}}/>
+                        {/* <Button>리뷰 확인</Button> */}
+                    </Link>
                 )}
             </td>
+            <td><CloseCircleTwoTone onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)} twoToneColor="#eb2f96" style={{fontSize:'20px'}}/></td>
         </tr>
     })
 
@@ -73,9 +81,8 @@ function FavoritePage() {
                 <thead>
                     <tr>
                         <th>Movie Title</th>
-                        <th>Movie RunTime</th>
-                        <th>Remove from Favorite</th>
                         <th>Review</th>
+                        <th>Remove</th>
                     </tr>
                 </thead>
                 <tbody>
