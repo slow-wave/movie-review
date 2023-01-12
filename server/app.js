4c0e852434d9d31caf49ee96d5997d6ed5a34e13
slow-wave <memoon_mj@icloud.com>
2023-01-12 20:24:35 +0900

    change mongodb connection config

--- server/app.js
+++ server/app.js
@@ -1,27 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require('cors')
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
-// 1. mongoose 모듈 가져오기
-var mongoose = require('mongoose');
-// 2. testDB 세팅
-mongoose.connect('mongodb://localhost/test');
-// 3. 연결된 testDB 사용
-var db = mongoose.connection;
-// 4. 연결 실패
-db.on('error', function(){
-    console.log('Connection Failed!');
-});
-// 5. 연결 성공
-db.once('open', function() {
-    console.log('Connected!');
-});
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, { dbName: "movie" })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.log("MongoDB failed", err);
+  });
 
-app.use(cors())
+app.use(cors());
 
 //to not get any deprecation warning or error
 //support parsing of application/x-www-form-urlencoded post data
@@ -31,20 +24,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/favorite', require('./routes/favorite'));
-app.use('/api/comment', require('./routes/comment'));
-app.use('/api/like', require('./routes/like'));
-app.use('/api/review', require('./routes/review'));
-app.use('/api/movie', require('./routes/movie'))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/favorite", require("./routes/favorite"));
+app.use("/api/comment", require("./routes/comment"));
+app.use("/api/like", require("./routes/like"));
+app.use("/api/review", require("./routes/review"));
+app.use("/api/movie", require("./routes/movie"));
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
-  // Set static folder   
+  // Set static folder
   // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
 
@@ -54,8 +46,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server Listening on ${port}`)
+  console.log(`Server Listening on ${port}`);
 });
--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://pumpkin:Su4JXXHki9zGxJFS@cluster0.v3ltnbq.mongodb.net/?retryWrites=true&w=majority'
+    mongoURI: "mongodb+srv://pumpkin:Su4JXXHki9zGxJFS@cluster0.v3ltnbq.mongodb.net/?retryWrites=true&w=majority"
 }
