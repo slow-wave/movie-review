ca085bb2782fc809aaacce09ad286c77c0eaafa4
slow-wave <memoon_mj@icloud.com>
2023-01-18 20:59:01 +0900

    update movie info

--- client/src/components/views/MovieDetail/Sections/MovieInfo.js
+++ client/src/components/views/MovieDetail/Sections/MovieInfo.js
@@ -1,4 +1,4 @@
-import { Descriptions } from "antd";
+import { Descriptions, Tag, Badge } from "antd";
 import React from "react";
 
 function MovieInfo(props) {
@@ -7,26 +7,31 @@ function MovieInfo(props) {
   return (
     <div>
       <h2 style={{ fontSize: "1.2rem" }}>Movie Info</h2>
-      <Descriptions bordered>
-        <Descriptions.Item label="Title">
-          {movie.original_title}
-        </Descriptions.Item>
-        <Descriptions.Item label="release_date">
-          {movie.release_date}
-        </Descriptions.Item>
-        <Descriptions.Item label="revenue">{movie.revenue}</Descriptions.Item>
-        <Descriptions.Item label="runtime">{movie.runtime}</Descriptions.Item>
-        <Descriptions.Item label="vote_average" span={2}>
-          {movie.vote_average}
-        </Descriptions.Item>
-        <Descriptions.Item label="vote_count">
-          {movie.vote_count}
-        </Descriptions.Item>
-        <Descriptions.Item label="status">{movie.status}</Descriptions.Item>
-        <Descriptions.Item label="popularity">
-          {movie.popularity}
-        </Descriptions.Item>
-      </Descriptions>
+      <div style={{ marginTop: "5%" }}>
+        <Descriptions title="summary" bordered>
+          <Descriptions.Item label="Title">
+            {movie.original_title}
+          </Descriptions.Item>
+          <Descriptions.Item label="release_date">
+            {movie.release_date}
+          </Descriptions.Item>
+          <Descriptions.Item label="Score">
+            <Badge
+              style={{ backgroundColor: "#379392" }}
+              count={movie.vote_average}
+            ></Badge>
+          </Descriptions.Item>
+          <Descriptions.Item label="status">{movie.status}</Descriptions.Item>
+          <Descriptions.Item label="runtime">
+            {movie.runtime >= 60 && parseInt(movie.runtime / 60) + "H "}
+            {movie.runtime % 60}M
+          </Descriptions.Item>
+          <Descriptions.Item label="genres">
+            {movie.genres &&
+              movie.genres.map((el) => <Tag color="geekblue">{el.name}</Tag>)}
+          </Descriptions.Item>
+        </Descriptions>
+      </div>
     </div>
   );
 }
